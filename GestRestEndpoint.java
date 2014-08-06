fd3ef1c99772aec09a40ae8b386ee050b455198b
daniel carrocera <danielcarrocera@gmail.com>
2014-08-06 23:24:22 +0200

    Revert "change done"
    This reverts commit c009d9db0408a9546e60fa3e21f08def72b7fd79.

--- GestRestEndpoint.java
+++ GestRestEndpoint.java
@@ -15,7 +15,7 @@ public interface GestRestEndpoint {
 	@WebMethod
 	@RequestWrapper(className="com.components.ws.WebMethodTest", localName="webMethodTest" , partName="webMethodTest" , targetNamespace="http://ws.components.com/")
 	@ResponseWrapper(className="com.components.ws.WebMethodTestResponse")
-	com.components.ws.impl.User webMethodTest2(String param1, String param2, String param3, String param4);
+	com.components.ws.impl.User webMethodTest(String param1, String param2, String param3, String param4);
 	
 
 }
